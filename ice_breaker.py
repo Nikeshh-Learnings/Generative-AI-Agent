00abf7bcb142ef9aa850de0035cb3945a73c7d93
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-05 15:12:05 -0400

    Twitter tweets integration

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,30 +2,35 @@ from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
-
 from third_parties.linkedin import scrape_linkedin_profile
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
+from third_parties.twitter import scrape_user_tweets
 
-def get_details(name: str) -> str:
-    linkedin_url = linkedin_lookup_agent(name=name)
-    linkedin_data = scrape_linkedin_profile(url=linkedin_url)
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(url=linkedin_username, mock=True)
 
-    template = """
-    given the Linkedin information {info} about a person I want you to create:
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, mock=True)
+
+    summary_template = """
+    given the information about a person from linkedin {information},
+    and their latest twitter posts {twitter_posts} I want you to create:
     1. A short summary
-    2. two interesting facts about them
+    2. two interesting facts about them 
+
+    Use both information from twitter and Linkedin
     """
-    prompt_template = PromptTemplate(
-        input_variables=["information"], template=template
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information", "twitter_posts"], template=summary_template
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    chain = LLMChain(llm=llm, prompt=prompt_template)
-    res = chain.invoke(input={"info": linkedin_data})
-
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+    res = chain.invoke(input={"information": linkedin_data, "twitter_posts": tweets})
     print(res)
-
 
 if __name__ == "__main__":
     load_dotenv()
-    get_details(name="Nikeshh")
+    ice_break_with(name="Nikeshh")
