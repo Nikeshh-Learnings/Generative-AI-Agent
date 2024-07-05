d89888d1d40c9cc1318ee6b83829f11b54170ecd
Nikeshh <nikeshhbaskaran01@gmail.com>
2024-07-05 14:44:48 -0400

    Linkedin lookup and Agent changes

--- ice_breaker.py
+++ ice_breaker.py
@@ -2,28 +2,30 @@ from dotenv import load_dotenv
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
-from third_parties.linkedin import scrape_linkedin_profile
 
-if __name__ == "__main__":
-    load_dotenv()
+from third_parties.linkedin import scrape_linkedin_profile
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
+def get_details(name: str) -> str:
+    linkedin_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(url=linkedin_url)
 
     template = """
-    given the Linkedin data {data} about a person I want you to create:
+    given the Linkedin information {info} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
-
     prompt_template = PromptTemplate(
-        input_variables=["data"], template=template
+        input_variables=["information"], template=template
     )
 
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-
     chain = LLMChain(llm=llm, prompt=prompt_template)
-    data = scrape_linkedin_profile(
-        url="https://www.linkedin.com/in/nikeshh/",
-        mock=True,
-    )
-    response = chain.invoke(input={"data": data})
+    res = chain.invoke(input={"info": linkedin_data})
 
-    print(response)
+    print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    get_details(name="Nikeshh")
